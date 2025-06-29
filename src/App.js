e6ba4185a76678a3670307eb14e9c23b588b3621
Douglas <douglasd123@hotmail.es>
2025-06-28 22:33:24 -0600

    Primer commit - Proyecto React Gamerzone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import Navbar from "./components/Navbar";
+import Carousel from "./components/Carousel";
+import About from "./components/About";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+import './App.css'; // aquí puedes importar los estilos si quieres
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Carousel />
+      <About />
+      <Services />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
